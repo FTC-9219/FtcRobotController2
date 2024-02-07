6584cade4aff4bc48ba39c1f16a8fba1fe576518
vienna-hev <stardog727@gmail.com>
2024-02-07 15:59:03 -0500

    no time for webcams...
    - gonna fix things tomorrow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop2.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.TouchSensor;
 
 @TeleOp
 public class Teleop2 extends LinearOpMode {
